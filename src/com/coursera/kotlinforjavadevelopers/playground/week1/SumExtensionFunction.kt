a6c2e4bf51e579f0cc3fa4c05aa902a2bc6761b0
Anthony Accioly <a.accioly@7rtc.com>
2018-11-01 21:49:59 +0000

    Week 1: Sum as an extension function

---
+++ src/com/coursera/kotlinforjavadevelopers/playground/week1/SumExtensionFunction.kt
@@ -0,0 +1,14 @@
+package com.coursera.kotlinforjavadevelopers.playground.week1
+
+fun List<Int>.sum(): Int {
+    var result = 0
+    for (i in this) {
+        result += i
+    }
+    return result
+}
+
+fun main(args: Array<String>) {
+    val sum = listOf(1, 2, 3).sum()
+    println(sum)    // 6
+}
