d2734ffe002c1584e9fec3be16334f56aa67a7dd
Anthony Accioly <a.accioly@7rtc.com>
2018-11-02 18:21:38 +0000

    Week 2: Mastermind in a functional style

---
+++ src/com/coursera/kotlinforjavadevelopers/playground/week2/MastermindFunctional.kt
@@ -0,0 +1,18 @@
+package com.coursera.kotlinforjavadevelopers.playground.week2
+
+data class Evaluation(val positions: Int, val letters: Int)
+
+fun evaluateGuess(secret: String, guess: String): Evaluation {
+    val positions = secret.zip(guess).count { (a, b) -> a == b }
+    val commonLetters = "ABCDEF".sumBy { ch ->
+        Math.min(secret.count { sCh -> sCh == ch }, guess.count { gCh -> gCh == ch })
+    }
+    return Evaluation(positions, commonLetters - positions)
+}
+
+fun main(args: Array<String>) {
+    val result = Evaluation(positions = 1, letters = 1)
+    evaluateGuess("BCDF", "ACEB") eq result
+    evaluateGuess("AAAF", "ABCA") eq result
+    evaluateGuess("ABCA", "AAAF") eq result
+}
