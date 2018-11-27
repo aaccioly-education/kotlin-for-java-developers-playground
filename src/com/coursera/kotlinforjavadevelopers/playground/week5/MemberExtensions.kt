ac13f78a3d5cf67ce351c2db69075525a34ecf70
Anthony Accioly <a.accioly@7rtc.com>
2018-11-27 20:36:41 +0000

    Week 5: Member Extensions

---
+++ src/com/coursera/kotlinforjavadevelopers/playground/week5/MemberExtensions.kt
@@ -0,0 +1,26 @@
+package com.coursera.kotlinforjavadevelopers.playground.week5
+
+import com.coursera.kotlinforjavadevelopers.playground.common.eq
+
+class Words {
+    private val list = mutableListOf<String>()
+
+    fun String.record() {
+        list += this
+    }
+
+    operator fun String.unaryPlus() = record()
+
+    override fun toString() = list.toString()
+
+}
+
+fun main(args: Array<String>) {
+    val words = Words()
+    with(words) {
+        // The following two lines should compile:
+        "one".record()
+        +"two"
+    }
+    words.toString() eq "[one, two]"
+}
